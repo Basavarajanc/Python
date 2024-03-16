f624fae8bc8c92db1e194858c67c3558c5f21454
BasavarajaCN <basavaraja.nagarajappa.chalagere@polestar.com>
2024-03-16 13:02:10 +0100

    fix: review comments

--- lesson/sum3.py
+++ lesson/sum3.py
@@ -3,5 +3,5 @@ num2 = int(input("Enter num2 : "))
 num3 = int(input("Enter num2 : "))
 
 
-sum = num1+ num2 + num3
-print("Sum is ", sum)
+sum = num1 + num2 + num3
+print("Sum of 3 numbers are  ", sum)
