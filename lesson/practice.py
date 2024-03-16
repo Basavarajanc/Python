90480c24d8e60ad72dbf2845c08d189274f97be2
BasavarajaCN <basavaraja.nagarajappa.chalagere@polestar.com>
2024-03-16 12:17:13 +0100

    feat: basic variables, strings, conditional practice

---
+++ lesson/practice.py
@@ -0,0 +1,5 @@
+firstName = input("input the first name :")
+
+print("first name is " + firstName + " and length is " + str(len(firstName)))
+
+print("Occurance of $ in the given string is " + str(firstName.count("$")))
---
+++ lesson/strings.py
@@ -0,0 +1,11 @@
+testString = "i am a coder, i dont have life"
+
+print(len(testString))
+
+print("Sliced string ",testString[0:12])
+
+print("Cpitalize string ",testString.capitalize())
+
+print("Cpitalize string ",testString.endswith("fe"))
+
+print("Cpitalize string ",testString.endswith("fe"))
